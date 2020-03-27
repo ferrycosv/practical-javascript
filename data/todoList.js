58ae3b6d1820c3e0514cae0203fd1e6140e8b192
Francisco Fernández <ferrycosv@gmail.com>
2020-03-27 02:48:35 +0100

    Completed addTodo functionality and render the list to screen

--- data/todoList.js
+++ data/todoList.js
@@ -1,3 +1,41 @@
 const todoList = {
-    todos: [],
-}
+  todos: [],
+  addTodo: function(event) {
+    if (event.keyCode === 13 && event.target.value !== "") {
+      const inputText = event.target.value;
+      this.todos.push({
+        todoText: inputText,
+        completed: false
+      });
+      event.target.value = "";
+      this.displayTodos();
+      log.push({
+        handler: 'addTodo',
+        todoList: JSON.parse(JSON.stringify(this))
+      });
+    }
+  },
+  displayTodos: function() {
+    const parent = document.getElementById("todoDiv");
+    const head = parent.children[0];
+    parent.innerHTML = "";
+    parent.appendChild(head);
+    this.todos.forEach(element => {
+        const listTodo = document.createElement("div");
+        listTodo.className = "todolist";
+        const checkTodo = document.createElement("i");
+        if (element.completed) {
+            checkTodo.className = "fa fa-check-circle";
+        } else {
+            checkTodo.className = "fa fa-check-circle gray";
+        }
+        listTodo.appendChild(checkTodo);
+        const inputTodo = document.createElement("input");
+        inputTodo.type = "text";
+        inputTodo.value = element.todoText;
+        //inputTodo.setAttribute("onkeyup", "todoList.addTodo(event)");
+        listTodo.appendChild(inputTodo);
+        parent.appendChild(listTodo);
+      });
+  },
+};
