127e7eff6cb6e88bcdec0947f040bd13a0269daf
Francisco Fernández <ferrycosv@gmail.com>
2020-03-27 15:31:49 +0100

    Completed changeTodo functionality with confirm to save changes

--- data/todoList.js
+++ data/todoList.js
@@ -1,41 +1,40 @@
 const todoList = {
   todos: [],
-  addTodo: function(event) {
-    if (event.keyCode === 13 && event.target.value !== "") {
-      const inputText = event.target.value;
-      this.todos.push({
-        todoText: inputText,
-        completed: false
-      });
-      event.target.value = "";
-      this.displayTodos();
-      log.push({
-        handler: 'addTodo',
-        todoList: JSON.parse(JSON.stringify(this))
-      });
-    }
+  addTodo: function(inputText) {
+    this.todos.push({
+      todoText: inputText,
+      completed: false
+    });
+  },
+  changeTodo: function(position,todoText){
+    this.todos[position].todoText = todoText;
   },
   displayTodos: function() {
+    let i = 0;
     const parent = document.getElementById("todoDiv");
     const head = parent.children[0];
     parent.innerHTML = "";
     parent.appendChild(head);
     this.todos.forEach(element => {
-        const listTodo = document.createElement("div");
-        listTodo.className = "todolist";
-        const checkTodo = document.createElement("i");
-        if (element.completed) {
-            checkTodo.className = "fa fa-check-circle";
-        } else {
-            checkTodo.className = "fa fa-check-circle gray";
-        }
-        listTodo.appendChild(checkTodo);
-        const inputTodo = document.createElement("input");
-        inputTodo.type = "text";
-        inputTodo.value = element.todoText;
-        //inputTodo.setAttribute("onkeyup", "todoList.addTodo(event)");
-        listTodo.appendChild(inputTodo);
-        parent.appendChild(listTodo);
-      });
-  },
+      const listTodo = document.createElement("div");
+      listTodo.className = "todolist";
+      const checkTodo = document.createElement("i");
+      if (element.completed) {
+        checkTodo.className = "fa fa-check-circle";
+      } else {
+        checkTodo.className = "fa fa-check-circle gray";
+      }
+      checkTodo.setAttribute("data-position", i);
+      listTodo.appendChild(checkTodo);
+      const inputTodo = document.createElement("input");
+      inputTodo.type = "text";
+      inputTodo.value = element.todoText;
+      inputTodo.setAttribute("data-position", i);
+      inputTodo.setAttribute("onkeyup", "handler.changeTodo(event)");
+      inputTodo.setAttribute("onchange", "handler.saveChanges(event)");
+      listTodo.appendChild(inputTodo);
+      parent.appendChild(listTodo);
+      i++;
+    });
+  }
 };
